349870a044f260fc68033cb6847e0ae40ad3efe3
manfrinne <m4nfrinne@gmail.com>
2020-09-01 17:42:26 -0300

    Animated menu setup

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const server = express();
 
+server.use(express.static('public'));
+
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
