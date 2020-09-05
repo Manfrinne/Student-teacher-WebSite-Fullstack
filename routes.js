84b49d50d9ee7b3fe143cd8f7c477d378ad54f6c
manfrinne <m4nfrinne@gmail.com>
2020-09-05 20:54:31 -0300

    List teachers index page

--- routes.js
+++ routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get("/", function(req, res) {
-    return res.redirect("teachers");
+  return res.redirect("teachers");
 });
 
 routes.get("/teachers", function(req, res) {
-    return res.render("teachers/show");
+  return res.render("teachers/index");
+});
+
+routes.get("/teachers/create", function(req, res) {
+  return res.render("teachers/create");
 });
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const server = express();
-const routers = require('./routes')
+const routers = require('./routes');
 
 server.use(express.static('public'));
 
-server.use(routers)
+server.use(routers);
 
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-    express: server,
-    autoescape: false,
-    noCache: true
-});
-
-server.get("/", function(req, res) {
-    return res.render("layout");
+  express: server,
+  autoescape: false,
+  noCache: true
 });
 
 server.listen(7070, function() {
-    console.log("server RUN!");
+  console.log("server RUN!");
 });
 
