0700aac87470a269fa6a69afe6181251985bc5e5
manfrinne <m4nfrinne@gmail.com>
2020-09-08 23:49:36 -0300

    Unstructure data send on req.body;

--- controllers/teachers.js
+++ controllers/teachers.js
@@ -16,13 +16,29 @@ exports.create = function(req, res) {
 
 exports.post = function(req, res) {
   const keys = Object.keys(req.body);
+
+  let {avatar_url, name, birth, academic_level, class_type, disciplines, create_at, id} = req.body;
+
   for (key of keys) {
     if (req.body[key] == "") {
       return res.send("ERROR, FAVOR PREENCHER TODOS OS CAMPOS!");
     };
   };
 
-  data.teachers.push(req.body);
+  create_at = Date.now(create_at);
+  birth = Date.parse(birth);
+  id = Number(data.teachers.length + 1);
+
+  data.teachers.push({
+    avatar_url,
+    name,
+    birth,
+    academic_level,
+    class_type,
+    disciplines,
+    create_at,
+    id,
+  });
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
     if (err) {return res.send("ERRO AO ESCREVER O ARQUIVO!")};
