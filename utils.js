bf083f7a8512139831f01925883584e90771d16f
manfrinne <m4nfrinne@gmail.com>
2020-09-25 13:39:42 -0300

    Correct bug in the Data function and configure logic of the tag select from academic_level;

--- utils.js
+++ utils.js
@@ -1,16 +1,16 @@
 module.exports = {
   age: function age(timestamp) {
-    const today = new Date();
-    const birthDay = new Date(timestamp);
+    const today = new Date()
+    const birthDate = new Date(timestamp)
 
-    let age = today.getFullYear() - birthDay.getFullYear();
-    const month = birthDay.getMonth();
+    let age = today.getFullYear() - birthDate.getFullYear()
+    const month = today.getMonth() - birthDate.getMonth()
 
-    if (month >= today.getMonth() && today.getDay() < birthDay.getDay()) {
-      age = age - 1;
-    };
+    if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
+        age = age - 1
+    }
 
-    return age;
+    return age
   },
 
   date: function date(timestamp) {
