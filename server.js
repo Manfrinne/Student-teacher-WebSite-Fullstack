3a0fa319419ef845b565b3b7b1e065ec6bead956
manfrinne <m4nfrinne@gmail.com>
2020-09-24 21:12:49 -0300

    Fix method-override bug;

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const server = express();
 const routers = require('./routes');
+const methodOverride = require('method-override');
 
 server.use(express.urlencoded({extended: true}));
 
 server.use(express.static('public'));
 
+server.use(methodOverride('_method'));
 server.use(routers);
 
 server.set("view engine", "njk");
